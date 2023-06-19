96e8efcc95966a6a42fa9e553520b1085771db20
Amit Kumar <112743124+Lucretia37@users.noreply.github.com>
2023-06-19 19:58:49 +0530

    Create complex operation with 100 loops.sol

---
+++ complex operation with 100 loops.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+contract ComplexContract {
+    function performComplexOperation() public {
+        for (uint256 i = 0; i < 100; i++) {
+            // Perform some computationally expensive operation
+            // ...
+        }
+    }
+}
